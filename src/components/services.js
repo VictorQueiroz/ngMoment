49eb80d947dc3e67056f4428493ac9d09b28095e
Victor Queiroz <victorcqueirozg@gmail.com>
2014-08-26 20:17:42 -0300

    initial commit

---
+++ src/components/services.js
@@ -0,0 +1,8 @@
+(function () {
+	'use strict';
+
+	angular
+		.module('ngMoment/Services', [
+			'ngMoment/Service'
+		]);
+})();
---
+++ src/module.js
@@ -0,0 +1,9 @@
+(function () {
+	'use strict';
+
+	angular
+		.module('ngMoment', [
+			'ngMoment/Services',
+			'ngMoment/Directives'
+		]);
+})();
