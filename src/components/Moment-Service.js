2238af4b48dc7400a98013a56ae0f5f8989e2f63
Victor Queiroz <victorcqueirozg@gmail.com>
2014-09-03 01:53:19 -0300

    Revision 1.0.3: moment removed from global variables

--- src/components/Moment-Service.js
+++ src/components/Moment-Service.js
@@ -1,10 +1,20 @@
-(function () {
+(function (window) {
   'use strict';
+
+	var $moment;
+
+	if(window.hasOwnProperty('moment')) {
+		$moment = angular.copy(window.moment);
+
+		if($moment().hasOwnProperty('format') || window.hasOwnProperty('moment')) {
+			window.moment = undefined;
+		}
+	}  
   
   angular
     .module('ngMoment/Service', [])
     
-    .factory('$moment', ['$window', function ($window) {
-      return $window.moment;
+    .factory('$moment', [function () {
+      return $moment;
     }]);
-})();
+})(window);
--- test/components/Moment-Service-Spec.js
+++ test/components/Moment-Service-Spec.js
@@ -7,12 +7,13 @@
 	MODULE_DIRECTIVES = [];
 
 	describe(MODULE_NAME + ' module', function () {
-		var $moment;
+		var $moment, $window;
 
 		beforeEach(module(MODULE_NAME));
 
 		beforeEach(inject(function ($injector) {
 			$moment = $injector.get('$moment');
+			$window = $injector.get('$window');
 		}));
 
 		it('should return moment', function () {
@@ -25,6 +26,30 @@
 			expect(typeof date).toBe('string');
 			expect($moment(date).subtract(1, 'year').fromNow()).toBe('a year ago');
 			expect($moment(date).subtract(1, 'day').fromNow()).toBe('a day ago');
+		});
+
+		it('should not be available on $window', function () {
+			expect($window.moment).toBe(undefined);
+		});
+
+		it('should change the locale', function () {
+			var date = new Date('Wed Sep 03 2014 00:00:00 GMT-0300 (BRT)');
+
+			$moment.locale('en');
+
+			expect($moment(date).format('DD MMM YY')).toBe('03 Sep 14');
+
+			$moment.locale('pt-br');
+
+			expect($moment(date).format('DD MMM YY')).toBe('03 set 14');
+		});
+
+		it('should keep the changed locale', function () {
+			var date = new Date('Wed Sep 03 2014 00:00:00 GMT-0300 (BRT)');
+
+			expect($moment(date)
+				.add(500, 'months')
+				.format('DD MMM YY')).toBe('03 mai 56');
 		});
 	});
 })();
